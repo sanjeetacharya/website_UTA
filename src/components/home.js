8b3352704779d5a13f91ef0d629427cbcd6cc1ad
Sanjeet Acharya <sanjeetacharya1@gmail.com>
2021-02-18 13:23:47 -0600

    2/18

---
+++ src/components/home.js
@@ -0,0 +1,10 @@
+import react from 'react';
+
+
+function Login(){
+<h1> 
+    <button> test Button</button>
+</h1>
+
+}
+export default Login;
---
+++ src/components/nav.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import button from react-bootstrap/button;
+
+/*funciton Nav(){
+return{
+    <nav class = "navbar navbar-expand-lg navbar-light bg-light">
+        <button class = "navbar-toggler" type = "button" data-toggle = "collapse" datar-target = "#navMainMenu" aria-aria-controls = "navMainMenu"
+}*/
+
+
+}
+export default Nav;
