04a3622ad00c8f6fdbc91745e1ed4d8f15cbebbb
Sanjeet Acharya <sanjeetacharya1@gmail.com>
2021-04-05 09:03:25 -0500

    4/5

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -43,7 +43,7 @@ function Navbar() {
             </li>
             <li className='nav-item'>
               <Link
-                to='/services'
+                to='/Events'
                 className='nav-links'
                 onClick={closeMobileMenu}
               >
@@ -52,7 +52,7 @@ function Navbar() {
             </li>
             <li className='nav-item'>
               <Link
-                to='/services'
+                to='/Blog'
                 className='nav-links'
                 onClick={closeMobileMenu}
               >
