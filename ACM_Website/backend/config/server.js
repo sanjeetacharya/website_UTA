09544cf7671825eb3adf2b139c25e67a944aca6b
Bojil Ivanov <33699106+IvanovBojil@users.noreply.github.com>
2021-04-30 19:19:05 -0500

    Added Final Code

---
+++ ACM_Website/backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '25cbc617988fdce42beae897881fd5c9'),
+    },
+  },
+});
---
+++ ACM_Website/backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '302e6eea-ab5f-496d-8e8b-dfaf13dda60b'
+};
